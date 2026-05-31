02ac459e306a9ed6e3587210a17a54724f22b5a1
Parikshit Thakur <parikshitthakur11@gmail.com>
2026-05-31 17:07:48 +0530

    odd number triangle pattern

---
+++ PATTERN/odd_number_triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter the no of elements:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=2*i-1;j=j+2)
+        {
+            printf("%d ",j);
+            
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
