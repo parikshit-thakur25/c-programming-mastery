cf703901d9d347363b9f144076143c5f48a418fc
parikshit-thakur25 <parikshitthakur11@gmail.com>
2026-05-31 16:03:12 +0530

    Abasic even/odd number checker-conditional

--- C-Basics/if_else_basic.c
+++ C-Basics/if_else_basic.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int x;
+    
+    printf("Enter a number of choice: ");
+    scanf("%d", &x);
+    
+    if (x % 2 == 0) {
+        printf("It is an even number\n");
+    } else {
+        printf("It is an odd number\n");
+    }
+    
+    return 0;
+}
